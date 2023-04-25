349db46ac6ca7509386c74efe611decca70aa135
huynhdev24 <huynh.dev.24@gmail.com>
2023-04-25 17:19:37 +0700

    config owner routing and owner, app modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,30 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { MenuComponent } from './menu/menu.component';
+import { OwnerCreateComponent } from './owner/owner-create/owner-create.component';
+import { OwnerDeleteComponent } from './owner/owner-delete/owner-delete.component';
+import { OwnerDetailsComponent } from './owner/owner-details/owner-details.component';
+import { OwnerListComponent } from './owner/owner-list/owner-list.component';
+import { OwnerUpdateComponent } from './owner/owner-update/owner-update.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { OwnerAccountsComponent } from './owner/owner-details/owner-accounts/owner-accounts.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    MenuComponent
+    MenuComponent,
+    OwnerCreateComponent,
+    OwnerDeleteComponent,
+    OwnerDetailsComponent,
+    OwnerListComponent,
+    OwnerUpdateComponent,
+    OwnerAccountsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
